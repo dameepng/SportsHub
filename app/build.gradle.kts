0c15d74c2cf8d9de79e51b6bf904e6f4e0e537c8
dameepng <ahmaddamanhuri45@gmail.com>
2026-05-30 19:55:06 +0700

    Enable app debug obfuscation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,13 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
